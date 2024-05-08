bdde177486c18bbd1811f702d0782ad52adedbde
Kural10489 <kuralanban.karthi@aspiresys.com>
2024-05-09 00:37:48 +0530

    Added necessary package and set port to 3000

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const dotenv=require("dotenv");
+const cors = require("cors");
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 // Enable CORS for all origins
 app.use(cors({
@@ -14,6 +15,16 @@ app.use(cors({
 
 // Using body parser to access json data 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // config !
 require('./config/db.config')
+
+// Routes
+
+const teamRoute=require('./routes/team.route')(app);
+
+app.listen(port,()=>{
+  console.log(`Server running in ${port}`);
+})
+module.exports = app;
