25fa64cf189a2ece31906a019ad820cdd073b4b5
Kural10489 <kuralanban.karthi@aspiresys.com>
2024-05-23 19:36:07 +0530

    Added New Feature /view-result

--- controllers/result.controller.js
+++ controllers/result.controller.js
@@ -3,7 +3,7 @@ const resultService=require("../service/result.service");
 exports.processResult = async (req,res,next) =>{
    try{
     const userId = req.params.userId;
-    const result = resultService.processResultService(userId);
+    const result =await resultService.processResultService(userId);
     if(result){
       res.status(200).send({
         message: "Your fantasy Team Points are proccessed !",
@@ -16,4 +16,21 @@ exports.processResult = async (req,res,next) =>{
          message: err.message || "Some error occurred while fetching users."
        });
    }
+}
+exports.viewTeamResult = async (req,res,next) =>{
+  try{
+   const result = await resultService.viewTeamResultService();
+   if(result){
+     res.status(200).send({
+       status:1,
+       message: "Fantasy Team Points are featched Successfully !",
+       result:result
+     });
+   }
+  } 
+  catch(err){
+     res.status(500).send({
+        message: err.message || "Some error occurred while fetching users."
+      });
+  }
 }
